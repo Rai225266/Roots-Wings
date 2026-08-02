786ead1e071736be46eeea4912a0ec1eb5285a22
Rai225266 <raisourabh1005@gmail.com>
2026-08-02 22:27:24 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -10,7 +10,8 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  dateStrings: true
+  dateStrings: true,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : undefined
 });
 
 pool.getConnection()
